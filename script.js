ace2848dd049cbe0e673ac9776bc3efc6ca2ce82
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-11-06 18:30:12 -0300

    finalizado a parte de css

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function toggleInfo() {
-    const moreInfo = document.getElementById("moreInfo");
-    moreInfo.style.display = moreInfo.style.display === "none" || moreInfo.style.display === "" ? "block" : "none";
-  }
-  
+  const moreInfo = document.getElementById("moreInfo");
+  moreInfo.style.display = moreInfo.style.display === "none" || moreInfo.style.display === "" ? "block" : "none";
+}
